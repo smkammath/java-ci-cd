f3a458bce674dc62a3d51559efbec95771837563
smkammath <smkammath@gmail.com>
2025-04-29 15:32:43 +0000

    Add calculator module with basic operations and tests

---
+++ java-ci-cd/unitconverter/src/main/java/com/example/unitconverter/UnitConverter.java
@@ -0,0 +1,10 @@
+package com.example.unitconverter;
+public class UnitConverter {
+public double milesToKilometers(double miles) {
+return miles * 1.60934;
+}
+public double kilogramsToPounds(double kilograms) {
+return kilograms * 2.20462;
+}
+}
+
